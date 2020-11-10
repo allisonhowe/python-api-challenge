789d683f8190db6452dd7b0cb40664d8983612f8
Allison Howe <allisonhowe77@gmail.com>
2020-11-09 20:21:42 -0500

    final

--- VacationPy/api_keys.py
+++ VacationPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "1a91132ae7bce50ddc4dfa949a8ba4f8"
+weather_api_key = "API KEY HERE!"
 
 # Google API Key
-g_key = "AIzaSyBuHU0_eQRQAAy9g0aLs2A9NayW84iGx-I"
+g_key = "API KEY HERE!"
